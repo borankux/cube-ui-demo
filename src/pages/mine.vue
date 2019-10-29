de873e2abf305c7fabd970fb21310022c9a1b654
Frank <mablat@ebay.com>
2019-10-30 05:52:22 +0800

    add mine

--- src/pages/mine.vue
+++ src/pages/mine.vue
@@ -1,6 +1,51 @@
 <template>
     <div class="page">
-      我的
+      <div class="profile-box">
+        <img class="avatar" src="../assets/imgs/ic_user.png" alt="">
+        <h2 class="name">登录/注册</h2>
+
+        <div class="badge-holder">
+          <div class="badge">
+            <h1>125</h1>
+            <h3>积分</h3>
+          </div>
+
+          <div class="badge">
+            <h1>320</h1>
+            <h3>浏览</h3>
+          </div>
+
+          <div class="badge">
+            <h1>22</h1>
+            <h3>收藏</h3>
+          </div>
+        </div>
+        <div class="row-list">
+          <div class="row">
+            <i class="start cubeic-mall"></i>
+            <span>我的订单</span>
+            <i class="end cubeic-arrow"></i>
+          </div>
+
+          <div class="row">
+            <i class="start cubeic-person"></i>
+            <span>我的资料</span>
+            <i class="end cubeic-arrow"></i>
+          </div>
+
+          <div class="row">
+            <i class="start cubeic-camera"></i>
+            <span>我的账户</span>
+            <i class="end cubeic-arrow"></i>
+          </div>
+
+          <div class="row">
+            <i class="start cubeic-setting"></i>
+            <span>系统设置</span>
+            <i class="end cubeic-arrow"></i>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -11,5 +56,61 @@
 </script>
 
 <style scoped>
+  .avatar {
+    display: block;
+    margin: 20px auto;
+    width: 100px;
+    height: 100px;
+    border: 1px solid #ccc;
+    border-radius: 100px;
+    padding: 10px;
+  }
 
+  .name {
+    text-align: center;
+    margin-top: 20px;
+    font-size: 1.5em;
+    font-weight: bolder;
+  }
+
+  .row-list {
+    margin-top: 20px;
+  }
+
+  .row {
+    height: 50px;
+    line-height: 50px;
+  }
+
+  .row .start {
+    margin:0 20px;
+    float: left;
+    font-size: 1.5em;
+  }
+
+  .row .end {
+    float: right;
+    margin-right: 20px;
+  }
+
+  .badge-holder {
+    margin: 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+
+  .badge {
+    margin: 10px 20px;
+  }
+
+  .badge h1 {
+    text-align: center;
+    font-size: 2em;
+  }
+
+  .badge h3 {
+    text-align: center;
+    margin-top: 5px;
+  }
 </style>
