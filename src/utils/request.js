acc25728d94cf3d3b96057193fc4e45d2cb31bb6
Frank <mablat@ebay.com>
2020-03-21 07:27:05 +0800

    another happy ending

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const requests = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? "http://106.14.35.196:8081": "http://localhost:8080"
+  baseURL: process.env.NODE_ENV === 'production' ? "https://jiazheng.mirzat.cn": "http://localhost:8080"
 });
 
 export default requests;
